0425ed997a49cb30a706c7e8a13a20d8a3d3fddc
marto <carrizofm2010@gmail.com>
2014-10-29 09:55:12 -0300

    acuario con target pez, falta animaciones

--- implementation/Ikaros AcuarioTribuno V1/Assets/Scripts/ReproducirAnimacion.cs
+++ implementation/Ikaros AcuarioTribuno V1/Assets/Scripts/ReproducirAnimacion.cs
@@ -10,41 +10,24 @@ public class ReproducirAnimacion : MonoBehaviour {
 	public float tiempoAnimacion=0;
 	public float tiempoTotal=0;
 	public float velicidadAnimacion=0 ;
-	private bool banderaAcuario= true;
-	
 	//public int contador=0;
 	// Use this for initialization
-	
-	public Transform marker;
-	private DefaultTrackableEventHandler defaultTrackeableEventHandler;
-	private ReproducirSonido soundPlay;
-	
 	void Start () {
 	  animacion = modelo3D.GetComponentInChildren<Animation>();
 	  this.animarPersonaje();
-	
-	  defaultTrackeableEventHandler = marker.GetComponent<DefaultTrackableEventHandler>();
-	  soundPlay = GetComponent<ReproducirSonido>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-		soundPlay.playSoundAcuario();
-
-		if (defaultTrackeableEventHandler.GetFlagMarker()){
-			if(banderaAcuario){
-				soundPlay.playSoundAcuario();
-				banderaAcuario=false;
-			}
 		velicidadAnimacion=animacion["AnimacionShark"].speed;
 		tiempoAnimacion=animacion["AnimacionShark"].time;
 	   tiempoTotal=animacion["AnimacionShark"].length;
 		if(tiempoAnimacion==0){
 			this.animarPersonaje();
 		}	
-		
-		}
+	
 	}
 	
 	public void animarPersonaje(){
@@ -53,8 +36,6 @@ public class ReproducirAnimacion : MonoBehaviour {
 	    animacion["AnimacionShark"].wrapMode=WrapMode.Once;
 		animacion["AnimacionShark"].speed=speed;
 		animacion.Play("AnimacionShark");
-		audio.Play();
-
 	}
 
 }
